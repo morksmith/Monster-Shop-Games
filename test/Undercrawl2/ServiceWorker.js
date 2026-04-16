9ad3c79c5f05f05027fef6ec91c00712fa581fa1
Mark Smith <54671859+morksmith@users.noreply.github.com>
2026-04-16 18:42:15 +0800

    Updating to Undercrawl 2 version 0.0.9

--- test/Undercrawl2/ServiceWorker.js
+++ test/Undercrawl2/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Monster Shop Games-Undercrawl 2-0.0.8";
+const cacheName = "Monster Shop Games-Undercrawl 2-0.0.9";
 const contentToCache = [
-    "Build/Undercrawl 2 WebGL 0.0.8.loader.js",
-    "Build/Undercrawl 2 WebGL 0.0.8.framework.js",
-    "Build/Undercrawl 2 WebGL 0.0.8.data",
-    "Build/Undercrawl 2 WebGL 0.0.8.wasm",
+    "Build/Undercrawl 2 WebGL 0.0.9.loader.js",
+    "Build/Undercrawl 2 WebGL 0.0.9.framework.js",
+    "Build/Undercrawl 2 WebGL 0.0.9.data",
+    "Build/Undercrawl 2 WebGL 0.0.9.wasm",
     "TemplateData/style.css"
 
 ];
