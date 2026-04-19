8e1cd68a895ef6c7c91e3a0519904b6ab179d466
MarkSmithSentient <mark.smith@sencom.com.au>
2026-04-20 07:57:51 +0800

    Uploading version 0.1.1

--- test/Undercrawl2/ServiceWorker.js
+++ test/Undercrawl2/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Monster Shop Games-Undercrawl 2-0.1.0";
+const cacheName = "Monster Shop Games-Undercrawl 2-0.1.1";
 const contentToCache = [
-    "Build/Undercrawl 2 0.1.0.loader.js",
-    "Build/Undercrawl 2 0.1.0.framework.js",
-    "Build/Undercrawl 2 0.1.0.data",
-    "Build/Undercrawl 2 0.1.0.wasm",
+    "Build/Undercrawl 2 0.1.1.loader.js",
+    "Build/Undercrawl 2 0.1.1.framework.js",
+    "Build/Undercrawl 2 0.1.1.data",
+    "Build/Undercrawl 2 0.1.1.wasm",
     "TemplateData/style.css"
 
 ];
