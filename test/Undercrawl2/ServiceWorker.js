0fe86a0c21271d9f13b174bc5903410ffdd6c7a9
MarkSmithSentient <mark.smith@sencom.com.au>
2026-08-04 10:49:02 +0800

    Updating Grim Gaunlet to version 0.3.2

--- test/Undercrawl2/ServiceWorker.js
+++ test/Undercrawl2/ServiceWorker.js
@@ -1,9 +1,9 @@
-const cacheName = "Monster Shop Games-Undercrawl 2-0.3.1";
+const cacheName = "Monster Shop Games-Undercrawl 2-0.3.2";
 const contentToCache = [
-    "Build/Undercrawl 2 0.3.1.loader.js",
-    "Build/Undercrawl 2 0.3.1.framework.js",
-    "Build/Undercrawl 2 0.3.1.data",
-    "Build/Undercrawl 2 0.3.1.wasm",
+    "Build/Undercrawl 2 0.3.2.loader.js",
+    "Build/Undercrawl 2 0.3.2.framework.js",
+    "Build/Undercrawl 2 0.3.2.data",
+    "Build/Undercrawl 2 0.3.2.wasm",
     "TemplateData/style.css"
 
 ];
